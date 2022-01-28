4c740f7b002120f1d6f1c346bd402516710c6521
Dom Aitken <93253286+DomAitken@users.noreply.github.com>
2022-01-28 13:19:24 +0000

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,6 @@
 pipeline{
         agent any
         stages{
-            stage('Build'){
-                steps{
-                    dir('/home/jenkins/.jenkins/workspace/ProjectPipeline') {
-                        sh "npm install"
-                        sh "npm start"
-                    }
-                }
-            }
             stage('Completion'){
                 steps{
                     echo "Build Completed."
